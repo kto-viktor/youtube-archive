245241d3a0602c4b76a3d1a7cf10cf5cfa31c2ef
eygonb <eygonb@gmail.com>
2023-07-07 19:50:36 +0300

    Enable configuration properties

--- backend/src/main/kotlin/com/trueprogrammers/youtubearchive/YoutubeArchiveApplication.kt
+++ backend/src/main/kotlin/com/trueprogrammers/youtubearchive/YoutubeArchiveApplication.kt
@@ -1,21 +1,13 @@
 package com.trueprogrammers.youtubearchive
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties::class)
 class YoutubeArchiveApplication
 
 fun main(args: Array<String>) {
 	runApplication<YoutubeArchiveApplication>(*args)
-}
-
-@Configuration
-class WebConfiguration : WebMvcConfigurer {
-	override fun addCorsMappings(registry: CorsRegistry) {
-		registry.addMapping("/**").allowedMethods("*")
-	}
 }
