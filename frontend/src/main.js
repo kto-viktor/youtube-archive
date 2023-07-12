924295a6537d6414a1fe5aaddba00e0b02f93d6c
Artyom <awesomemdl95@gmail.com>
2023-07-12 22:05:52 +0300

    register api service globally

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import Service from '@/service/';
 
 const app = createApp(App);
+
+app.config.globalProperties.$ServiceApi = new Service();
 app.use(router)
 app.mount('#app');
