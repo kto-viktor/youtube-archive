bf1f30a4f0fcad5005706cebd49b442a06715af8
Artyom <awesomemdl95@gmail.com>
2023-07-15 23:41:05 +0300

    edit api service, added new

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -29,7 +29,7 @@ export default class {
 		}
 	}
 
-	async getMetadata(link) {
+	async getVideoMetadata(link) {
 		try {
 			const res = await api.get(`/video/metadata?url=${link}`);
 			return res;
@@ -42,6 +42,54 @@ export default class {
 	async saveVideo(data) {
 		try {
 			const res = await api.post(isDev ? '/video/archives/' : '/video', {
+				...data
+			});
+
+			return res;
+
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	async checkVideoStatus(id) {
+		try {
+			return await api.get(`/video/archives/${id}`);
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	async getAllPlaylists() {
+		try {
+			const res = await api.get(isDev ? '/playlist/archives' : '/playlist/archives?query=');
+			return res.data;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	async searchPlaylists(query) {
+		try {
+			const res = await api.get(`/playlist/archives?query=${query}`)
+			return res.data;
+		} catch (error) {
+			throw new Error(error)
+		}
+	}
+
+	async getPlaylistMetadata(query) {
+		try {
+			const res = await api.get(`/playlist/metadata?url=${query}`)
+			return res;
+		} catch (error) {
+			throw new Error(error)
+		}
+	}
+
+	async savePlaylist() {
+		try {
+			const res = await api.post('/playlist', {
 				data
 			});
 
@@ -52,9 +100,9 @@ export default class {
 		}
 	}
 
-	async checkStatus(id) {
+	async checkVideoListStatus(id) {
 		try {
-			return await api.get(`/archives/${id}`);
+			return await api.get(`/playlist/archives/${id}`);
 		} catch (error) {
 			throw new Error(error);
 		}
