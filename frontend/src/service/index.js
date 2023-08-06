584e4687f6509c64bbc85ddf598141c85a3c5449
eygonb <eygonb@gmail.com>
2023-08-06 21:55:49 +0300

    Fix catching error by deleting catch in index.js

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -12,99 +12,50 @@ const api = axios.create({
 
 export default class {
 	async getAllVideos() {
-		try {
-			const res = await api.get('/video/archives?query=');
-			return res.data;
-		} catch (error) {
-			throw new Error(error)
-		}
+		const res = await api.get('/video/archives?query=');
+		return res.data;
 	}
 
 	async searchVideos(query) {
-		try {
-			const res = await api.get(`/video/archives?query=${query}`)
-			return res.data;
-		} catch (error) {
-			throw new Error(error)
-		}
+		const res = await api.get(`/video/archives?query=${query}`)
+		return res.data;
 	}
 
 	async getVideoMetadata(link) {
-		try {
-			const res = await api.get(`/video/metadata?url=${link}`);
-			return res;
-
-		} catch (error) {
-			throw new Error(error);
-		}
+		return await api.get(`/video/metadata?url=${link}`);
 	}
 
 	async saveVideo(data) {
-		try {
-			const res = await api.post(isDev ? '/video/archives/' : '/video', {
-				...data
-			});
-
-			return res;
-
-		} catch (error) {
-			throw new Error(error);
-		}
+		return await api.post(isDev ? '/video/archives/' : '/video', {
+			...data
+		});
 	}
 
 	async checkVideoStatus(id) {
-		try {
-			return await api.get(`/video/archives/${id}`);
-		} catch (error) {
-			throw new Error(error);
-		}
+		return await api.get(`/video/archives/${id}`);
 	}
 
 	async getAllPlaylists() {
-		try {
-			const res = await api.get(isDev ? '/playlist/archives' : '/playlist/archives?query=');
-			return res.data;
-		} catch (error) {
-			throw new Error(error);
-		}
+		const res = await api.get(isDev ? '/playlist/archives' : '/playlist/archives?query=');
+		return res.data;
 	}
 
 	async searchPlaylists(query) {
-		try {
-			const res = await api.get(`/playlist/archives?query=${query}`)
-			return res.data;
-		} catch (error) {
-			throw new Error(error)
-		}
+		const res = await api.get(`/playlist/archives?query=${query}`)
+		return res.data;
 	}
 
 	async getPlaylistMetadata(query) {
-		try {
-			const res = await api.get(`/playlist/metadata?url=${query}`)
-			return res;
-		} catch (error) {
-			throw new Error(error)
-		}
+		return await api.get(`/playlist/metadata?url=${query}`)
 	}
 
 	async savePlaylist() {
-		try {
-			const res = await api.post('/playlist', {
-				data
-			});
-
-			return res;
-
-		} catch (error) {
-			throw new Error(error);
-		}
+		return await api.post('/playlist', {
+			data
+		});
 	}
 
 	async checkVideoListStatus(id) {
-		try {
-			return await api.get(`/playlist/archives/${id}`);
-		} catch (error) {
-			throw new Error(error);
-		}
+		return await api.get(`/playlist/archives/${id}`);
 	}
 }
