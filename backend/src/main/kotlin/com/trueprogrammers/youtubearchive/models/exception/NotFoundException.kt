998e12af8dff2116ac2eec79da1e9682b9f900be
eygonb <eygonb@gmail.com>
2023-08-22 10:28:37 +0300

    Refactor to correct code style

--- backend/src/main/kotlin/com/trueprogrammers/youtubearchive/models/exception/NotFoundException.kt
+++ backend/src/main/kotlin/com/trueprogrammers/youtubearchive/models/exception/NotFoundException.kt
@@ -1,8 +1,8 @@
 package com.trueprogrammers.youtubearchive.models.exception
 
 class NotFoundException : Exception {
-    constructor(): super()
-    constructor(message: String?): super(message)
-    constructor(message: String?, cause: Throwable?): super(message, cause)
-    constructor(cause: Throwable?): super(cause)
+    constructor() : super()
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    constructor(cause: Throwable?) : super(cause)
 }
